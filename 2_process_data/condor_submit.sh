f65a32d5a251919fe688785607d9e5e96f3b5c65
Christopher Dalldorf <cdalldorf@gmail.com>
2024-10-03 13:26:46 -0400

    z

--- 2_process_data/condor_submit.sh
+++ 2_process_data/condor_submit.sh
@@ -2,18 +2,18 @@
 # setup proper pathing and permissions
 source /opt/bifxapps/miniconda3/etc/profile.d/conda.sh
 unset $PYTHONPATH
-export NXF_HOME=$HOME/.nextflow
 
 # activate my environment
 conda activate modulome
+export NXF_HOME=$HOME/.nextflow
 
 # record time, run nextflow
 printf "Start time: "; /bin/date
 printf "Job is running on node: "; /bin/hostname
 printf "Job running as user: "; /usr/bin/id
 printf "Job is running in directory: "; /bin/pwd
-echo NXF_HOME
-printf NXF_HOME
+echo $NXF_HOME
+printf $NXF_HOME
 nextflow run main.nf -profile local
 echo
 
