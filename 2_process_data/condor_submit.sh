9e152e77aa0c1525998901308efcb6e7256abe51
Christopher Dalldorf <cdalldorf@gmail.com>
2024-10-03 13:39:25 -0400

    z

--- 2_process_data/condor_submit.sh
+++ 2_process_data/condor_submit.sh
@@ -5,15 +5,14 @@ unset $PYTHONPATH
 
 # activate my environment
 conda activate modulome
-export NXF_HOME=$HOME/.nextflow
+export NXF_HOME=/home/glbrc.org/cdalldorf/.nextflow
 
 # record time, run nextflow
 printf "Start time: "; /bin/date
 printf "Job is running on node: "; /bin/hostname
 printf "Job running as user: "; /usr/bin/id
 printf "Job is running in directory: "; /bin/pwd
-echo $NXF_HOME
-printf $NXF_HOME
+echo
 nextflow run main.nf -profile local
 echo
 
