00f32cc6b9ee50081e68a0bd176c69bc1432928f
Christopher Dalldorf <cdalldorf@gmail.com>
2024-10-03 13:09:33 -0400

    modifying path for nextflow

--- 2_process_data/condor_submit.sh
+++ 2_process_data/condor_submit.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-# running nextflow
+# setup proper pathing
 source /opt/bifxapps/miniconda3/etc/profile.d/conda.sh
 unset $PYTHONPATH
+export NXF_HOME=$HOME/.nextflow
 
 # activate my environment
 conda activate modulome
 
+# record time, run nextflow
 printf "Start time: "; /bin/date
 printf "Job is running on node: "; /bin/hostname
 printf "Job running as user: "; /usr/bin/id
