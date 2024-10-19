2fa6ddac3ce23d2fa2027312f9ac0b6b40cc6fff
Christopher Dalldorf <cdalldorf@gmail.com>
2024-10-19 14:25:34 -0400

    adding condor_submit files for ICA

---
+++ 4_optICA/condor_submit.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# setup proper pathing and permissions
+source /opt/bifxapps/miniconda3/etc/profile.d/conda.sh
+unset $PYTHONPATH
+
+# activate my environment
+conda activate modulome
+export NXF_HOME=/home/glbrc.org/cdalldorf/.nextflow
+
+# record time, run nextflow
+printf "Start time: "; /bin/date
+printf "Job is running on node: "; /bin/hostname
+printf "Job running as user: "; /usr/bin/id
+printf "Job is running in directory: "; /bin/pwd
+echo
+./run_ica.sh -n 4 -o ../data/interim/ ../data/processed_data/log_tpm_norm.csv
+echo
+
+# wrap up
+echo "Science complete!"
+printf "End time: "; /bin/date
+conda deactivate
