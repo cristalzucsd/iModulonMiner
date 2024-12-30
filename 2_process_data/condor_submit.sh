6af096d89d2271ec0d983c2a8b3cca41ac101ecd
cdalldorf <cdalldorf@scarcity-submit.glbrc.org>
2024-12-30 08:57:26 -0600

    ran on xylose genes for relevant sample

--- 2_process_data/condor_submit.sh
+++ 2_process_data/condor_submit.sh
@@ -13,6 +13,7 @@ rm -rf trace/*
 # setup privelages
 chmod +x ./bin/*.py
 chmod +x ./*.sh
+chmod +x ./bin/*.sh
 
 # record time, run nextflow
 printf "Start time: "; /bin/date
@@ -20,7 +21,7 @@ printf "Job is running on node: "; /bin/hostname
 printf "Job running as user: "; /usr/bin/id
 printf "Job is running in directory: "; /bin/pwd
 echo
-nextflow run main.nf -profile local -c nextflow.config -resume
+nextflow run main.nf -profile local -c nextflow.config -resume -with-trace
 echo
 
 # wrap up
